2dac1aa0dccb1fd8b8582bbcc0e460116d9daa74
Erick Montañez <codeams@gmail.com>
2016-10-03 18:01:22 -0500

    Agregando estilos a la pantalla de inicio de sesión

--- index.php
+++ index.php
@@ -1,32 +1,40 @@
 <!doctype html>
-<html>
-  <head>
-    <meta charset='utf-8'>
-    <title>Iniciar sesión</title>
-    <link rel='stylesheet' href='assets/styles.css'>
-  </head>
-  <body>
+<html><head>
 
-    <h1>Iniciar sesión</h1>
+  <meta charset='utf-8'>
+  <title>Iniciar sesión</title>
+  <link rel='stylesheet' href='assets/styles.css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600' rel='stylesheet'>
 
-    <p>Utilice su cuenta UADY.<br>
-    <a href='#'>¿Qué es esto?</a></p>
+</head><body>
 
-    <form id='login' name='login' action=''>
+  <div class='content-wrapper'>
 
-      <input type='text' id='username' name='username' placeholder='Clave de profesor'>
+    <div class='title'>Iniciar sesión</div>
+
+    <div class='instructions'>
+      <p>Utilice su cuenta UADY.</p>
+      <p><a href='#'>¿Qué es esto?</a></p>
+    </div>
+
+    <div class='form'><form id='login' name='login' action=''>
+
+      <input type='text' id='username' name='username' placeholder='Clave de profesor' autocomplete='off'>
       <input type='password' id='password' name='password' placeholder='Contraseña'>
-      <label><input type='checkbox' id='keepSession' name='keepSession' checked='checked'> Mantener mi sesión iniciada</label>
+      <label for='keepSession'><input type='checkbox' id='keepSession' name='keepSession'> Mantener mi sesión iniciada</label>
       <input type='submit' id='submit' name='submit' value='Iniciar sesión'>
 
+    </form></div>
+
+    <div class='recover-account-links'>
       <p><a href='#'>He olvidado mi clave de profesor</a></p>
       <p><a href='#'>He olvidado mi contraseña</a></p>
+    </div>
 
-    </form>
+  </div>
 
-    <script src='assets/jquery.min.js'></script>
-    <script src='assets/event-handler.js'></script>
-    <script src='assets/ajax-handler.js'></script>
+  <script src='assets/jquery.min.js'></script>
+  <script src='assets/event-handler.js'></script>
+  <script src='assets/ajax-handler.js'></script>
 
-  </body>
-</html>
+</body></html>
